0239bcd503d85155a7fd0d898c374f0572837640
yuiitoh <yuizho3@gmail.com>
2017-06-22 00:19:36 +0900

    modify ResultList.js.
    add isbn check.

--- app/containers/ResultList.js
+++ app/containers/ResultList.js
@@ -10,6 +10,7 @@ import * as Progress from 'react-native-progress';
 import ApiClient from '../lib/apiClient'
 import Book from '../components/Book'
 import Config from 'react-native-config'
+import ISBN from 'isbnjs'
 
 function goBackDialog(title, content, navigation) {
   Alert.alert(title, content,
@@ -24,37 +25,44 @@ class ResultList extends Component {
 
   constructor(props) {
     super(props)
-    // TODO: ISBNかどうかをチェックする必要がある
-    const code = props.navigation.state.params.code
     this.state = { searching: false, items: [], errMsg: '' }
   }
 
   componentDidMount() {
     this.setState({searching: true})
-    // fetch e-book data form API.
-    Promise.all([ApiClient.get(Config.API_URL + this.props.navigation.state.params.code)])
-      .then((result) => {
-        const ebooks = result[0].ebooks
-        if (!ebooks.length) {
-          goBackDialog('E-Book検索結果',
-                       '読み取った本の電子書籍はみつかりませんでした。',
-                       this.props.navigation)
-        } else {
-          this.setState({searching: false, items: ebooks})
-        }
-      })
-      .catch((ex) => {
-        console.log(ex)
-        if (ex.status === 500 || ex.status === 503) {
-          goBackDialog('サーバエラー',
-                       '現在サーバが混み合っています。少し時間をおいてから、再度バーコードを読み取ってください。',
-                       this.props.navigation)
-        } else {
-          goBackDialog('通信エラー',
-                       '通信エラーが発生しました。通信状態を確認して、再度バーコードを読み取ってください。',
-                       this.props.navigation)
-        }
-      })
+    const isbn = this.props.navigation.state.params.code
+    const parsedISBN = ISBN.parse(isbn)
+    if (!parsedISBN || !parsedISBN.isIsbn13()) {
+      // isbn check
+      goBackDialog('バーコードエラー',
+                   '書籍上部のISBNコードを読み取ってください。別のバーコードが読み取られてしまう場合は、指などで隠した上で読み取りを行ってください',
+                   this.props.navigation)
+    } else {
+      // fetch e-book data form API.
+      Promise.all([ApiClient.get(Config.API_URL + this.props.navigation.state.params.code)])
+        .then((result) => {
+          const ebooks = result[0].ebooks
+          if (!ebooks.length) {
+            goBackDialog('E-Book検索結果',
+                         '読み取った本の電子書籍はみつかりませんでした。',
+                         this.props.navigation)
+          } else {
+            this.setState({searching: false, items: ebooks})
+          }
+        })
+        .catch((ex) => {
+          console.log(ex)
+          if (ex.status === 500 || ex.status === 503) {
+            goBackDialog('サーバエラー',
+                         '現在サーバが混み合っています。少し時間をおいてから、再度バーコードを読み取ってください。',
+                         this.props.navigation)
+          } else {
+            goBackDialog('通信エラー',
+                         '通信エラーが発生しました。通信状態を確認して、再度バーコードを読み取ってください。',
+                         this.props.navigation)
+          }
+        })
+    }
   }
 
   openURL(url) {
